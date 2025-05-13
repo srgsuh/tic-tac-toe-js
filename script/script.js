989f9a5825b1fb5abfdb48d7825ac0b5575fdb93
srgsuh <srg.suhoverhov@gmail.com>
2025-05-13 11:58:41 +0300

    Game logic transferred to game.js. TicTacToe syntax changed to class

--- script/script.js
+++ script/script.js
@@ -23,67 +23,12 @@ function sendMessages(...messages) {
     );
 }
 
-function TicTacToe(fieldSize = 3, winSize = fieldSize, blank = '') {
-    this.blank = blank;
-    this.size = fieldSize;
-    this.winSize = winSize;
-    this.emptyCells = this.size * this.size
-    this.field = Array.from({length: this.size}, () => Array(this.size).fill(this.blank));
-    this.currentPlayer = 'X';
-    this.idlePlayer = 'O';
-    this.changePlayer = function() {
-        [this.currentPlayer, this.idlePlayer] = [this.idlePlayer, this.currentPlayer];
-    };
-    // Returns true if the move ends the game (win or draw)
-    this.move = function(row, col, onMove, onGameEnd) {
-        if (row < 0 || row >= this.size || col < 0 || col >= this.size || this.field[row][col] !== this.blank) {
-            return false;
-        }
-        onMove(this.currentPlayer, this.idlePlayer);
-        this.field[row][col] = this.currentPlayer;
-        --this.emptyCells;
-        if (this.checkWinner(row, col, this.currentPlayer)) {
-            onGameEnd(this.currentPlayer);
-            return true;
-        }
-        if (this.emptyCells === 0) {
-            onGameEnd(null);
-            return true;
-        }
-        this.changePlayer();
-        return false;
-    };
-
-    this.checkWinner = function(rowIndex, colIndex, player) {
-        //Counts the maximum consecutive occurrences of a value in an array.
-        function countMax(vector, value) {
-            let max = 0, current = 0;
-            for (let i = 0; i < vector.length; i++) {
-                current = (vector[i] === value)? current + 1 : 0;
-                max = Math.max(max, current);
-            }
-            return max;
-        }
-        let vector;
-        vector = this.field[rowIndex];
-        if (countMax(vector, player) >= this.winSize) return true;
-        vector = this.field.flatMap((row) => row[colIndex]);
-        if (countMax(vector, player) >= this.winSize) return true;
-        if (rowIndex === colIndex) {
-            vector = this.field.flatMap( (row, idx) => row[idx]);
-            if (countMax(vector, player) >= this.winSize) return true;
-        }
-        if (rowIndex + colIndex === this.size - 1) {
-            vector = this.field.flatMap( (row, idx) => row[this.size - 1 -idx]);
-            if (countMax(vector, player) >= this.winSize) return true;
-        }
-        return false;
-    }
-}
 const game = new TicTacToe(gameSize);
+console.log(game);
 
 sendMessages(`Dear players! Welcome to this ${gameSize}×${gameSize} tic-tac-toe game!`
 ,`Player ${game.currentPlayer}'s move`);
+
 const cells = document.querySelectorAll(".cell");
 cells.forEach((cell, index) => {
     let rowIndex = Math.floor(index / gameSize);
