dc01ea8e86294eca6f35c9fe42ba013e7052a503
srgsuh <srg.suhoverhov@gmail.com>
2025-05-14 16:32:17 +0300

    Logic now managed by a single big object of class GameBoard. Minor improvements

--- script/script.js
+++ script/script.js
@@ -1,101 +1,140 @@
 'use strict';
 
-let boardSize = -1;
-let game = null;
-let board = null;
-let cells = null;
+const CELL_STYLES = {
+    DEFAULT: {
+        background: 'white',
+        color: 'black'
+    },
+    WINNING: {
+        background: 'green',
+        color: 'white'
+    }
+};
 
-resetGame();
-document.getElementById("new-game").addEventListener("click", resetGame);
+class GameBoard {
+    constructor(containerSelector) {
+        this.container = document.querySelector(containerSelector);
+        this.board = null;
+        this.cells = null;
+        this.size = -1;
+    }
 
-// Generation of the board
-function resetGame() {
-    let newBoardSize = +document.getElementById("board-size").value;
-    if (newBoardSize !== boardSize) {
-        if (cells) {
-            cells.forEach(row => {
-                row.forEach(cell => {
-                    cell.removeEventListener("click", cell.customClickHandler);
-                });
+    initialize(size) {
+        this.size = size;
+        this.createBoard();
+        this.createCells();
+        this.setupBoardLayout();
+    }
+
+    createBoard() {
+        if (this.board) {
+            this.board.remove();
+        }
+        this.board = document.createElement("div");
+        this.board.className = "field";
+        this.container.appendChild(this.board);
+    }
+
+    createCells() {
+        this.cells = Array.from({ length: this.size }, (_, row) =>
+            Array.from({ length: this.size }, (_, col) => {
+                const cell = document.createElement("button");
+                cell.className = "cell";
+                this.board.appendChild(cell);
+                return cell;
+            })
+        );
+    }
+
+    setupBoardLayout() {
+        this.board.style.gridTemplateColumns = `repeat(${this.size}, 1fr)`;
+        this.board.style.gridTemplateRows = `repeat(${this.size}, 1fr)`;
+    }
+
+    setupCellsClickHandlers(clickHandler) {
+        this.cells.forEach((row, rowIndex) => {
+            row.forEach((cell, colIndex) => {
+                cell.currentClickHandler = clickHandler(cell, rowIndex, colIndex);
+                cell.addEventListener("click", cell.currentClickHandler);
             });
-        }
-        cells = null;
-        console.log(board);
-        if (board) {
-            board.remove();
-            board = document.createElement("div");
-            board.className = "field";
-            document.querySelector(".container").appendChild(board);
-        }
-        else {
-            board =document.querySelector(".field");
-        }
-        console.log(board);
-        boardSize = newBoardSize;
-        generateField(boardSize);
-        addEventListeners();
-    } else {
-        refreshCells();
+        })
     }
-    game = new TicTacToe(boardSize);
-}
 
-function refreshCells() {
-    cells.forEach((row) => row.forEach(
-        cell => {
-            cell.disabled = false;
-            cell.textContent = GAME_SYMBOLS.BLANK;
-            cell.style.backgroundColor = "white";
-            cell.style.color = "black";
+    deleteCellsClickHandlers() {
+        if (this.cells) {
+            this.cells.forEach((row) => row.forEach(
+                cell => {
+                    cell.removeEventListener("click", cell.currentClickHandler);
+                    cell.currentClickHandler = null;
+                }
+            ));
         }
-    ));
-}
+    }
 
-function generateField(size = 3) {
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    cells = [];
-    for (let r = 0; r < size; r++) {
-        cells.push([]);
-        for (let c = 0; c < size; c++) {
-            const cell = document.createElement("button");
-            cell.className = "cell";
-            board.appendChild(cell);
-            cells[r].push(cell);
-        }
+    refreshAllCells() {
+        this.cells.forEach((row) => row.forEach(
+            cell => {
+                cell.disabled = false;
+                cell.textContent = GAME_SYMBOLS.BLANK;
+                this.applyCellStyle(cell, CELL_STYLES.DEFAULT);
+            }
+        ));
+    }
+
+    disableAllCells() {
+        this.cells.forEach((row) => row.forEach(
+            cell => cell.disabled = true
+        ));
+    }
+
+    highlightWinningCells(winningCells) {
+        winningCells.forEach(([row, column]) => {
+            const cell = this.cells[row][column];
+            this.applyCellStyle(cell, CELL_STYLES.WINNING);
+        });
+    }
+
+    applyCellStyle(cell, style) {
+        cell.style.backgroundColor = style.background;
+        cell.style.color = style.color;
     }
 }
 
-function createCellClickHandler(rowIndex, colIndex) {
+function createCellClickHandler(cell, rowIndex, colIndex) {
     return function() {
-        const cell = cells[rowIndex][colIndex];
-        if (cell.disabled) {
-            return;
-        }
+        console.log(rowIndex, colIndex, cell);
         cell.textContent = game.currentPlayer;
         cell.disabled = true;
         const gameState = game.makeMove(rowIndex, colIndex);
         if (gameState.gameEnd) {
-            cells.forEach((row) => row.forEach(c => c.disabled = true));
+            gameBoard.disableAllCells();
             if (gameState.winner === null) {
                 alert("Draw!");
             } else {
-                for (let i = 0; i < gameState.winningCells.length; i++) {
-                    cells[gameState.winningCells[i][0]][gameState.winningCells[i][1]].style.backgroundColor = "green";
-                    cells[gameState.winningCells[i][0]][gameState.winningCells[i][1]].style.color = "white";
-                }
+                gameBoard.highlightWinningCells(gameState.winningCells);
                 alert(`Player ${gameState.winner} has won!`);
             }
         }
-    };
+    }
 }
 
-
-function addEventListeners() {
-    cells.forEach((row, rowIndex) => {
-        row.forEach((cell, colIndex) => {
-            cell.customClickHandler = createCellClickHandler(rowIndex, colIndex);
-            cell.addEventListener("click", cell.customClickHandler);
-        });
-    });
+function resetGame() {
+    const boardSize = +document.getElementById("board-size").value;
+    console.log(boardSize);
+    game.init(boardSize);
+    console.log(game);
+    if (boardSize === gameBoard.size) {
+        gameBoard.refreshAllCells()
+    }
+    else {
+        gameBoard.deleteCellsClickHandlers();
+        gameBoard.initialize(boardSize);
+        gameBoard.setupCellsClickHandlers(createCellClickHandler);
+    }
+    console.log(gameBoard);
 }
+
+const gameBoard = new GameBoard(".container");
+const game = new TicTacToe();
+document.getElementById("new-game").addEventListener("click", resetGame);
+resetGame();
